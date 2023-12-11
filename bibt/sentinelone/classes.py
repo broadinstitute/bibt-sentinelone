def64c7ba8276fb8595b9d5733c049b3a365e145
Matthew OBrien <mobrien@broadinstitute.org>
2023-12-11 17:51:52 -0500

    client config

--- bibt/sentinelone/classes.py
+++ bibt/sentinelone/classes.py
@@ -6,5 +6,7 @@ _LOGGER = logging.getLogger(__name__)
 class Client:
     """ """
 
-    def __init__(self):
-        pass
+    def __init__(self, username, password, domain):
+        self.username = username
+        self.password = password
+        self.domain = domain
